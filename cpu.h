6a5e293a012b26991d681e7322feaaf8e703e52a
Alberto Barbieri <albertobarbieri@AirdiAlberto4.homenet.telecomitalia.it>
2023-12-26 16:16:15 +0100

    prova push

---
+++ cpu.h
@@ -0,0 +1,18 @@
+#ifndef CPU_H
+#define CPU_H
+
+#include <iostream>
+
+class Cpu : Player{
+    
+    void buy_land(int cost);
+    void buy_house (int cost);
+    void buy_hotel(int cost);
+
+};
+
+
+
+
+
+#endif //CPU_H
